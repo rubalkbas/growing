7c5745a4d803b0f405298295ddbd1ac952263691
JAV <prueba@test.com>
2024-09-02 00:03:23 -0600

    cambios provicionales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,14 +12,14 @@ import { FirstPassGuard } from './core/auth/guards/firstPass.guard';
 export const appRoutes: Route[] = [
 
     // Redirect empty path to '/example'
-    {path: '', pathMatch : 'full', redirectTo: 'home'},
+    {path: '', pathMatch : 'full', redirectTo: 'growing'},
 
     // Redirect signed-in user to the '/example'
     //
     // After the user signs in, the sign-in page will redirect the user to the 'signed-in-redirect'
     // path. Below is another redirection for that path to redirect the user to the desired
     // location. This is a small convenience to keep all main routes together here on this file.
-    {path: 'signed-in-redirect', pathMatch : 'full', redirectTo: 'home'},
+    {path: 'signed-in-redirect', pathMatch : 'full', redirectTo: 'growing'},
 
     // Auth routes for guests
     {
