937b8c4f3ae65df319890f596f4d50bcf550e428
rubalkbas <tucorreo@example.com>
2024-09-01 19:04:08 -0700

    cambios

--- src/environments/environment.desa.ts
+++ src/environments/environment.desa.ts
@@ -1,7 +1,9 @@
 export const environment = {
     production: false,
-    basePathUrl: 'http://177.225.210.68:8091',
-    socket: 'ws://177.225.210.68:8092/websocket'
+    basePathUrl: 'https://www.growingcapitalmaker.org:8091',
+    socket: 'wss://www.growingcapitalmaker.org:8092/websocket'
+     //   basePathUrl: 'https://177.225.210.68:8091',
+    //socket: 'ws://177.225.210.68:8092/websocket'
         //basePathUrl: 'http://177.225.210.68:8084'
     //basePathUrl: 'http://177.225.210.68:8084'
     // basePathUrl: 'http://104.192.6.36:8091',
