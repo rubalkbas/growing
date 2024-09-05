13557c5f51924987d59f718aa2f401a0b432c518
FABRICA <FABRICA@FABRICA.com>
2024-09-05 02:29:42 -0600

    EN

---
+++ src/app/modules/admin/clientes/abiertas-modal/apalancamiento/alternaInfo.component.ts
@@ -0,0 +1,152 @@
+import { Component, CSP_NONCE, Inject, OnInit } from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule, CurrencyPipe, DatePipe, NgClass } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { AlertService } from 'app/modules/services/alerts.service';
+import { User } from 'app/core/user/user.types';
+import { Subject, takeUntil } from 'rxjs';
+import { UserService } from 'app/core/user/user.service';
+import { Rol } from 'app/mock-api/common/interfaces/rol.interface';
+import { RolesService } from 'app/modules/services/roles.service';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { PerfilamientoService } from 'app/modules/services/perfilamientos.service';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { ClienteService } from '../../clientes.service';
+import Swal from 'sweetalert2';
+interface ViewValue {
+  value: number;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-alternaInfo-modal',
+  templateUrl: './alternaInfo.component.html',
+  styleUrls: ['./alternaInfo.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatIcon,
+    MatRadioModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatSortModule,
+    FormsModule,
+    MatButtonModule, MatIconModule, MatMenuModule, MatDividerModule, NgApexchartsModule, NgClass, MatProgressBarModule, CurrencyPipe, DatePipe, MatCardModule
+  ],
+})
+
+export class ApalancamientoModalComponent implements OnInit {
+  displayedColumns: string[] = ['tipoCompra', 'compra','valorUnidad', 'unidades','montoApuesta', 'variacion','gananciaPerdida', 'bloqueCompra' , 'fechaCreacion','accion' ];
+  userForm: FormGroup;
+  permisos = [];
+  newRol: any;
+  usuarioLoggeado: any;
+  private _unsubscribeAll: Subject<any> = new Subject<any>();
+  estados: ViewValue[] = [
+    { value: 1, viewValue: 'Activo' },
+    { value: 0, viewValue: 'Inactivo' },
+  ];
+  formCliente: FormGroup;
+ 
+  showAlert = false;
+  total = 0;
+  datasource = new MatTableDataSource<any>();
+  idUser: string;
+  monto: any = 0;
+  constructor(
+    public dialogRef: MatDialogRef<ApalancamientoModalComponent>,
+    private fb: FormBuilder,
+    private alertService: AlertService,
+    private clienteService: ClienteService,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+
+    this.userForm = this.fb.group({
+      apalancamiento1gana: ['', Validators.required],
+      apalancamiento2gana: ['', Validators.required],
+      apalancamiento1pierde: ['', Validators.required],
+      apalancamiento2pierde: ['', Validators.required],
+    
+    });
+
+    this.userForm.get('apalancamiento1gana').patchValue(this.data.data.unidades);
+    this.userForm.get('valorUnidad').patchValue(this.data.data.valorUnidad);
+    this.userForm.get('unidades').patchValue(this.data.data.unidades);
+    this.userForm.get('valorUnidad').patchValue(this.data.data.valorUnidad);
+
+    this.idUser = localStorage.getItem('idUserWrog');
+
+  }
+
+  ngOnInit(): void {
+  
+
+  } 
+
+  onClose(): void {
+    this.dialogRef.close(this.newRol);
+  }
+
+
+
+  onSubmit(): void {
+
+   this.data.data.unidades = this.userForm.get('unidades').value
+   this.data.data.valorUnidad = this.userForm.get('valorUnidad').value
+
+   this.clienteService.actualizaApuesta(this.data.data).subscribe({
+    next: (respuesta: any) => {
+ 
+      console.log('Respuesta completa: ', respuesta);
+      // Accediendo a la lista de areas de atención dentro de la respuesta
+      if (respuesta.estatus === 'OK') {
+
+        this.dialogRef.close(this.newRol);
+        this.alertService.success("Clientes","información alterada completamente")
+      } else {
+        console.log(
+          'La respuesta no contiene una lista válida de areas de atención.'
+        );
+      }
+    },
+    error: (error: Error) => {
+      console.error(error);
+    },
+  });
+
+  }
+
+
+
+}
